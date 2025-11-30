70cf614b9a91147789df0e0ab4d1bf5211e0d45d
siddhant <siddhantkumarupmanyu@gmail.com>
2025-11-30 23:33:39 +0530

    Implement `/cc` route parameter parsing:
    - Added support for extracting `lat` and `lon` query parameters.
    - Included validation for invalid or missing parameters, returning a `400` status.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
+const countriesGeoJson = await Bun.file("data/ne_50m_admin_0_countries.json").json()
+
+// for (const feature of countriesGeoJson) {
+//     console.log(`${feature.properties["ISO_A2_EH"]} - ${feature.properties["NAME"]}`)
+// }
+
+
 Bun.serve({
     routes: {
         "/ping": new Response("pong"),
         "/cc": (req) => {
+            const url = new URL(req.url).searchParams
+            const lat = parseFloat(url.get("lat")!)
+            const lon = parseFloat(url.get("lon")!)
+            if (isNaN(lat) || isNaN(lon)) {
+                return new Response("invalid params", {status: 400})
+            }
+            console.log(`${lat}, ${lon}`)
+            // const code = findCountry()
             const resp = {
                 "cc": "IN"
             }
